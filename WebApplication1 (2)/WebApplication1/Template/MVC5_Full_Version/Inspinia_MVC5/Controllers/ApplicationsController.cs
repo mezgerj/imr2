88e04e3606985a518e3ad5df24273e500decd987
mezgerj <mezger.10@gmail.com>
2015-07-12 11:28:02 -0400

    new
    new

---
+++ WebApplication1 (2)/WebApplication1/Template/MVC5_Full_Version/Inspinia_MVC5/Controllers/ApplicationsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inspinia_MVC5.Models;
+using System.Diagnostics;
+
+namespace Inspinia_MVC5.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private ScaffoldingContext db = new ScaffoldingContext();
+
+        // GET: /Applications/
+        public ActionResult Index()
+        {
+            return View(db.Applications.ToList());
+        }
+
+        // GET: /Applications/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Finds all the users with same company id as the application. 
+            IEnumerable<SelectListItem> users = from user in db.Supporters.ToList()
+                                                where user.Company_Id.Equals(application.Company_Id)
+                                                select new SelectListItem
+                                                {
+                                                    Text = user.FirstName + " " + user.LastName,
+                                                    Value = user.Supporter_Id.ToString()
+                                                };
+            ViewData["Supporter_Id"] = users;
+           
+            return View(application);
+        }
+
+        // GET: /Applications/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Applications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Application_Id,Name,Description,Company_Id,ApplicationToken")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Applications.Add(application);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(application);
+        }
+
+        // GET: /Applications/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: /Applications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Application_Id,Name,Description,Company_Id,ApplicationToken")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(application).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(application);
+        }
+
+        // GET: /Applications/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: /Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Application application = db.Applications.Find(id);
+            db.Applications.Remove(application);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AssignUser([Bind(Include = "Application_Id,Supporter_Id")] Application_Supporters model)
+        {
+            //TODO show error message for trying to add someone who is already there or only display people who aren't already there.
+            //Supporter currentSupporter = db.Supporters.Find(WebSecurity.CurrentUserId);
+
+            //if (application.Company_Id == currentSupporter.Company_Id || currentSupporter.Role_Id == 1)
+            //{
+             db.Application_Supporters.Add(model);
+
+                //application.Supporters.Add(db.Supporters.Find(model.Supporter_Id));
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = model.Application_Id });
+            /*
+            }
+            else
+            {
+                //TODO error page
+                return View();
+            } */
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveUser([Bind(Include = "Application_Id,Supporter_Id")] Application_Supporters model)
+        {
+          //TODO display error message for trying to remove someone who isn't there or only display people that can be removed.
+            Application_Supporters entry = db.Application_Supporters.Find(model.Application_Id, model.Supporter_Id);
+            db.Application_Supporters.Remove(entry);
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = model.Application_Id });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ WebApplication1 (2)/WebApplication1/Template/MVC5_Full_Version/Inspinia_MVC5/Controllers/SupportersController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inspinia_MVC5.Models;
+using Inspinia_MVC5.UtilityFolder;
+
+namespace Inspinia_MVC5.Controllers
+{
+    public class SupportersController : Controller
+    {
+        private ScaffoldingContext db = new ScaffoldingContext();
+
+        // GET: /Supporters/
+        public ActionResult Index()
+        {
+            return View(db.Supporters.ToList());
+        }
+
+        // GET: /Supporters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supporter supporter = db.Supporters.Find(id);
+            if (supporter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supporter);
+        }
+
+        // GET: /Supporters/Create
+        public ActionResult Create()
+        {
+            //Used to show the different role options in a drop down menu
+            //if (supporter.Role_Id == 1)
+            //{
+            ViewBag.Role_Id = new SelectList(db.Supporter_Roles, "Role_Id", "Name");
+            //ViewBag.Role = new SelectList(new[] { "SuperAdmin", "Admin", "Manager", "Supporter" });
+            /*}
+            else
+            {
+                ViewBag.Role = new SelectList(new[] { "Admin", "Manager", "Supporter" });
+            }*/
+            return View();
+        }
+
+        // POST: /Supporters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Supporter_Id,UserName,Company_Id,FirstName,LastName,Email,Role_Id")] Supporter supporter)
+        {
+            //Supporter supporter = db.Supporters.Find(WebSecurity.CurrentUserId);
+
+            //if (supporter.Role_Id == 1)
+            //{
+                //ViewBag.Role = new SelectList(new[] { "SuperAdmin", "Admin", "Manager", "Supporter" });
+            /*}
+            else
+            {
+                ViewBag.Role = new SelectList(new[] { "Admin", "Manager", "Supporter" });
+            }*/
+
+            if (ModelState.IsValid)
+            {
+
+                supporter.FirstTime = true;
+                string password = PasswordUtility.GenRandPassword(10);
+                supporter.Password = password;
+
+                bool valid = true;
+                foreach (Inspinia_MVC5.Models.Supporter sup in db.Supporters)
+                {
+                    if (sup.Email == supporter.Email)
+                    {
+                        TempData["msg"] = "<script>alert('Email matches an exisiting user's.');</script>";
+                        valid = false;
+                    }
+                }
+
+                if (valid == true) 
+                {
+                EmailUtility.SendUserCreationEmail(supporter.Email, supporter.LastName, supporter.UserName, password);
+                db.Supporters.Add(supporter);
+
+                db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(supporter);
+        }
+
+        // GET: /Supporters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supporter supporter = db.Supporters.Find(id);
+            if (supporter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supporter);
+        }
+
+        // POST: /Supporters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Supporter_Id,UserName,Company_Id,FirstName,LastName,Email,Phone,Street,City,State,Country,PostalCode,Password,FirstTime,Role_Id")] Supporter supporter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supporter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supporter);
+        }
+
+        // GET: /Supporters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supporter supporter = db.Supporters.Find(id);
+            if (supporter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supporter);
+        }
+
+        // POST: /Supporters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Supporter supporter = db.Supporters.Find(id);
+            db.Supporters.Remove(supporter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
